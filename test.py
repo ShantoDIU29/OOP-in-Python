483ab35fe88110192934b121ccba4a72af42fbea
Shanto Roy <165570551+ShantoDIU29@users.noreply.github.com>
2025-08-07 19:30:08 +0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+#from folder_name import class_name
+import constructor_instance_importclass
+
+d1=constructor_instance_importclass.Book("Opekkha","Humayun Ahmed") 
+d1.details()
+d1.set_price(255)
+d1.get_price()
+# print(x)
+d1.details()
