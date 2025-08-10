58367f5f8ace97b461e1d387b2c780462dff5536
Shanto Roy <165570551+ShantoDIU29@users.noreply.github.com>
2025-08-10 20:44:09 +0600

    Add files via upload

---
+++ when_method_overloading.py
@@ -0,0 +1,11 @@
+class my_calculator:
+  def product(self,num1,num2):
+    print(num1*num2)
+  def product(self,num1,num2,num3):
+    print(num1*num2*num3)
+#method holo kno class same name method defferent paramter atai method overloading 
+#same nama multiple method declare korta parbo na   
+#=======================
+c1=my_calculator()
+c1.product(4,5)
+c1.product(4,5,6)
