6b1f0481d140b823b5da46a6acc2a61e8827eb8a
Shanto Roy <165570551+ShantoDIU29@users.noreply.github.com>
2025-08-10 21:35:45 +0600

    Add files via upload

---
+++ when_Constrcuter_overloading.py
@@ -0,0 +1,11 @@
+class Student:
+  def __init__(self,name,id):
+    self.name=name
+    self.id=id
+  def __init__(self,name,id,cg):
+    self.name=name
+    self.id=id 
+    self.cg=cg 
+    
+s1=Student("shanto",33)
+s2=Student("Bob",11,3.95)
