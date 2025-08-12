4b513b5a38adc33c4e863cae1084eddfbb96d1c1
Shanto Roy <165570551+ShantoDIU29@users.noreply.github.com>
2025-08-12 10:34:15 +0600

    Encapsulation | Public & Private variable

---
+++  Encapsulation/public_private_Variable.py
@@ -0,0 +1,21 @@
+class Student:
+  def __init__(self,name,id):
+    self.name=name #instance variable
+    self.__id=id  #(__)private instance variable 
+  
+  def details(self): #instance method
+    print("Name:",self.name,"ID:",self.__id)  
+  def update_Id(self,ID):
+     if(ID>0):
+       self.__id=ID
+     else:
+       print("Invalid ID,enter id again")    
+s1=Student("Shanto",11)
+s2=Student("shimul",24)
+s1.__id=-66     # no update id bcz private
+# print(s1.__id)
+print(s1.__dict__)  #{'name': 'Shanto', '_Student__id': 11, '__id': -66}
+s1.update_Id(66)
+s1.details()
+s2.details()    
+    
