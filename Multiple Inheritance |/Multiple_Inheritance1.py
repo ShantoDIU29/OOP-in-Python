0e838e417634935810dbcf2e6aaf98d883b62f2d
shanto roy <shantosr3559@gmail.com>
2025-08-17 11:36:18 +0600

    Multiple Inheritance

---
+++ Multiple Inheritance |/Multiple_Inheritance1.py
@@ -0,0 +1,25 @@
+class A:
+  def __init__(self):
+    print("__init__ of class A ")
+    
+  def method1(Self):
+     print("method1 of class A") 
+     
+     
+class B:
+    def __init__(self):
+       print("__init__ of class B")
+    def method1(self):
+       print("method1 of class B")
+class C(A, B):
+  
+   def __init__(self):
+      # super().__init__()
+      #B.__init__(self)
+      print("__init__ of class C")
+      
+   def method2(self):
+     print("method of class C")          
+c1=C() 
+c1.method1() 
+B.method1(c1)          
